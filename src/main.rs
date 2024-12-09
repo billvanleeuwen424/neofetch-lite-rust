eb8ef879c6d75bbd321d4b0bbce51fdba5357e3a
billvanleeuwen424 <billvanleeuwen424@gmail.com>
2024-12-09 18:42:03 -0500

    structified it

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,24 @@
 use std::fs::File;
 use std::io::{BufRead, BufReader};
+
+struct SystemInfo {
+    cpu: Option<String>,
+}
+
+impl SystemInfo {
+    fn new() -> Self {
+        SystemInfo { cpu: None }
+    }
+}
+
 fn main() {
+    let mut sys_info = SystemInfo::new();
     // read in proc/cpuinfo
     let proc_file = File::open("/proc/cpuinfo").unwrap();
 
     let reader = BufReader::new(proc_file);
 
+    // read until
     for line in reader.lines() {
         let line = line.unwrap();
         if line.contains("model name") {
@@ -16,6 +29,8 @@ fn main() {
             // check that the vector is at least 2 long
             if model_name_set.len() >= 2 {
                 println!("{}", model_name_set[1].trim());
+
+                sys_info.cpu = Some(String::from(model_name_set[1].trim()));
             }
 
             break;
