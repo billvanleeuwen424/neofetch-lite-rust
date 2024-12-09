558549000a2f11da7ab5a99dc3e4f1a6afa96440
billvanleeuwen424 <billvanleeuwen424@gmail.com>
2024-12-09 17:39:25 -0500

    print the cpu model name

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,24 @@
-use std::fs;
-
+use std::fs::File;
+use std::io::{BufRead, BufReader};
 fn main() {
     // read in proc/cpuinfo
-    let proc_contents = fs::read_to_string("/proc/cpuinfo").unwrap();
+    let proc_file = File::open("/proc/cpuinfo").unwrap();
 
-    // print it
-    println!("{}", proc_contents);
+    let reader = BufReader::new(proc_file);
+
+    for line in reader.lines() {
+        let line = line.unwrap();
+        if line.contains("model name") {
+            // split the string on the ':', get the second item
+            let model_name_set: Vec<&str> = line.split(":").collect();
+
+            // get the string after the : if it exists
+            // check that the vector is at least 2 long
+            if model_name_set.len() >= 2 {
+                println!("{}", model_name_set[1].trim());
+            }
+
+            break;
+        }
+    }
 }
