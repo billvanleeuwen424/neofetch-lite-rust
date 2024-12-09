be8a27c198557b6e8b148b0cddf0e4bed221b7a3
billvanleeuwen424 <billvanleeuwen424@gmail.com>
2024-12-09 16:04:43 -0500

    reading in the cpuinfo

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
+use std::fs;
+
 fn main() {
-    println!("Hello, world!");
+    // read in proc/cpuinfo
+    let proc_contents = fs::read_to_string("/proc/cpuinfo").unwrap();
+
+    // print it
+    println!("{}", proc_contents);
 }
