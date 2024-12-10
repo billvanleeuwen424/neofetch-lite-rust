1ab3492542ecee7669dda5f6aa469aa28c9e6273
billvanleeuwen424 <billvanleeuwen424@gmail.com>
2024-12-10 17:57:52 -0500

    move the cpu info out of the function

--- src/main.rs
+++ src/main.rs
@@ -10,26 +10,26 @@ impl SystemInfo {
     fn new() -> Self {
         SystemInfo { cpu: None }
     }
+}
 
-    fn get_cpu_info(&mut self) {
-        let proc_file = File::open("/proc/cpuinfo").unwrap();
-        let reader = BufReader::new(proc_file);
+fn get_cpu_info(cpu_name: &mut Option<String>) {
+    let proc_file = File::open("/proc/cpuinfo").unwrap();
+    let reader = BufReader::new(proc_file);
 
-        for line in reader.lines() {
-            let line = line.unwrap();
-            if line.contains("model name") {
-                // split the string on the ':', get the second item
-                let model_name_set: Vec<&str> = line.split(":").collect();
+    for line in reader.lines() {
+        let line = line.unwrap();
+        if line.contains("model name") {
+            // split the string on the ':', get the second item
+            let model_name_set: Vec<&str> = line.split(":").collect();
 
-                // get the string after the : if it exists
-                // check that the vector is at least 2 long
-                if model_name_set.len() >= 2 {
-                    println!("{}", model_name_set[1].trim());
-                    self.cpu = Some(String::from(model_name_set[1].trim()));
-                }
-
-                break;
+            // get the string after the : if it exists
+            // check that the vector is at least 2 long
+            if model_name_set.len() >= 2 {
+                println!("{}", model_name_set[1].trim());
+                *cpu_name = Some(String::from(model_name_set[1].trim()));
             }
+
+            break;
         }
     }
 }
@@ -37,6 +37,5 @@ impl SystemInfo {
 fn main() {
     let mut sys_info = SystemInfo::new();
 
-    sys_info.get_cpu_info();
     println!("{:?}", sys_info);
 }
