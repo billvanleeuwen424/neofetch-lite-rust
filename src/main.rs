b0f09a5caaae732dfc5a89ffbd16e9b9bb23c460
billvanleeuwen424 <billvanleeuwen424@gmail.com>
2024-12-09 19:18:33 -0500

    moved function into the struct

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use std::fs::File;
 use std::io::{BufRead, BufReader};
 
+#[derive(Debug)]
 struct SystemInfo {
     cpu: Option<String>,
 }
@@ -9,31 +10,33 @@ impl SystemInfo {
     fn new() -> Self {
         SystemInfo { cpu: None }
     }
+
+    fn get_cpu_info(&mut self) {
+        let proc_file = File::open("/proc/cpuinfo").unwrap();
+        let reader = BufReader::new(proc_file);
+
+        for line in reader.lines() {
+            let line = line.unwrap();
+            if line.contains("model name") {
+                // split the string on the ':', get the second item
+                let model_name_set: Vec<&str> = line.split(":").collect();
+
+                // get the string after the : if it exists
+                // check that the vector is at least 2 long
+                if model_name_set.len() >= 2 {
+                    println!("{}", model_name_set[1].trim());
+                    self.cpu = Some(String::from(model_name_set[1].trim()));
+                }
+
+                break;
+            }
+        }
+    }
 }
 
 fn main() {
     let mut sys_info = SystemInfo::new();
-    // read in proc/cpuinfo
-    let proc_file = File::open("/proc/cpuinfo").unwrap();
 
-    let reader = BufReader::new(proc_file);
-
-    // read until
-    for line in reader.lines() {
-        let line = line.unwrap();
-        if line.contains("model name") {
-            // split the string on the ':', get the second item
-            let model_name_set: Vec<&str> = line.split(":").collect();
-
-            // get the string after the : if it exists
-            // check that the vector is at least 2 long
-            if model_name_set.len() >= 2 {
-                println!("{}", model_name_set[1].trim());
-
-                sys_info.cpu = Some(String::from(model_name_set[1].trim()));
-            }
-
-            break;
-        }
-    }
+    sys_info.get_cpu_info();
+    println!("{:?}", sys_info);
 }
